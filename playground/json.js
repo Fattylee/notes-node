ddf4e00df11e68040cdcc9f45acfaa363be6c658
Fatai Balogun <fattylee.remod@gmail.com>
2019-02-09 18:35:50 +0100

    complete a close look at JSON obj along side eexploring writeFileSync and readFileSync methods

---
+++ playground/json.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const noteObj = {
+  title: 'This is a title',
+  body: 'This is a body',
+};
+
+// converting object to a string for easy persistence
+
+const stringNote = JSON.stringify(noteObj);
+fs.writeFileSync('playground/note.json', stringNote);
+console.log('here')
+
+const strNote = fs.readFileSync('playground/note.json');
+console.log(strNote, typeof strNote);
+
+const objNote = JSON.parse(strNote);
+console.log(objNote.body)
